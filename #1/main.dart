a1818f83350c1c2f80edd2efd9a9275a22ae4ddb
Eduwardo Horibe <40374938+eduhoribe@users.noreply.github.com>
2020-03-19 21:41:11 -0300

    Update main.dart

--- #1/main.dart
+++ #1/main.dart
@@ -1,1 +1,108 @@
-path_holder
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Meu Software'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Meu Software"),
+        ),
+        body: Center(
+          child: Container(
+            color: Colors.orange,
+            margin: EdgeInsets.all(16),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Container(
+                    color: Colors.green,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          "Lista de Nomes:",
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        Text(
+                          "Alunos da USF",
+                          style: TextStyle(
+                            fontSize: 16
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Container(
+                    color: Colors.grey,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(80),
+
+                          child: Image.network("https://www.hercampus.com/sites/default/files/user_images/hKv3Fddh.jpg",width: 100,height: 100),
+                          
+                          ),
+                          Text(
+                            "Roberto Carlos",
+                            style: TextStyle(
+                              fontSize: 16
+                            ),
+                          )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            )
+
+          ),
+        ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem> [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text("Home")
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.note),
+            title: Text("Notas")
+          )
+        ],
+      )
+    );
+  }
+}
